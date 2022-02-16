f9f66febff581e64db5e742ce362fd552af227c2
camilodi1999 <60122434+camilodi1999@users.noreply.github.com>
2022-02-16 02:28:49 -0500

    fetch front

--- back_django/back_django/views.py
+++ back_django/back_django/views.py
@@ -8,9 +8,8 @@ zona_origen = data_frame['zone_0'].unique().tolist()
 modos_transport = data_frame['attr_0'].unique().tolist()
 
 def first_time(request):
-    print('entro')
     response = data_frame[(data_frame["zone_0"] == zona_origen[0]) & (data_frame["attr_0"] == modos_transport[0]) ]   
-    return HttpResponse(response.to_json(orient="index"))
+    return HttpResponse(response.to_json())
 
 def params(request):
     json_response = json.dumps({'zona':zona_origen, 'modos':modos_transport})
@@ -20,6 +19,5 @@ def params(request):
 def data(request):
     zona = request.GET.get('zona','')
     modo_T = request.GET.get('modo','')
-    print((data_frame["zone_0"] == int(zona)))
     response = data_frame[(data_frame["zone_0"] == int(zona)) & (data_frame["attr_0"] == modo_T) ]   
-    return HttpResponse(response.to_json(orient="index"))
+    return HttpResponse(response.to_json())
--- front-react/src/components/visualizacion.js
+++ front-react/src/components/visualizacion.js
@@ -1,4 +1,23 @@
+import React, { useState, useEffect } from "react";
+
 function Visualizacion() {
+  const url_django = "http://localhost:8000";
+  const [fields, setFields] = useState({});
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch(url_django + "/params")
+      .then((res) => res.json())
+      .then((json) => {
+        setFields(json);
+      })
+      .then(() => fetch(url_django + "/first"))
+      .then((res) => res.json())
+      .then((json) => setData(json));
+  }, []);
+
+  console.log(fields);
+  console.log(data);
   return (
     <div>
       <p>hola</p>
