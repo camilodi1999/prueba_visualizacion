effad28c0714a3bba6eb803cb484ec66ae978ad2
camilodi1999 <60122434+camilodi1999@users.noreply.github.com>
2022-02-20 20:09:14 -0500

    text

--- front-react/src/components/lineChart.js
+++ front-react/src/components/lineChart.js
@@ -7,48 +7,57 @@ function LineChart(props) {
   useEffect(() => {
     let svgElement = d3.select(svgRef.current);
     svgElement.selectAll("*").remove();
+
+    let margin = 200;
+    let width = 600 - margin;
+    let height = 500 - margin;
+
     let leftMargin = 70;
     let topMargin = 10;
 
     let xExtent = d3.extent(data, (d) => d.time);
-    let xScale = d3.scaleLinear().domain(xExtent).range([leftMargin, 900]);
+    let xScale = d3.scaleLinear().domain(xExtent).range([0, width]);
 
     let yMax = d3.max(data, (d) => d.value);
     let yScale = d3
       .scaleLinear()
-      .domain([d3.extent(data, (d) => d.value)[0], yMax + topMargin])
-      .range([400, 20]);
+      .domain(d3.extent(data, (d) => d.value))
+      .range([height, 0]);
 
     let xAxis = d3.axisBottom().scale(xScale);
 
-    svgElement
+    let g = svgElement
       .append("g")
+      .attr("transform", "translate(" + 100 + "," + 100 + ")");
+
+    g.append("g")
       .attr("class", "axis")
-      .attr("transform", "translate(0,420)")
+      .attr("transform", "translate(0," + height + ")")
       .call(xAxis)
       .append("text")
-      .attr("x", 420 / 2) //middle of the xAxis
-      .attr("y", "0") // a little bit below xAxis
+      .attr("y", height - 250)
+      .attr("x", width - 200)
+      .attr("text-anchor", "end")
+      .attr("stroke", "black")
       .text("Year");
 
     //yAxis and yAxis label
     let yAxis = d3.axisLeft().scale(yScale).ticks(10);
 
-    svgElement
-      .append("g")
-      .attr("class", "axis")
-      .attr("transform", `translate(${leftMargin},20)`) //use variable in translate
+    g.append("g")
       .call(yAxis)
       .append("text")
       .attr("transform", "rotate(-90)")
-      .attr("x", "-150")
-      .attr("y", "0")
-      .attr("text-anchor", "middle");
+      .attr("x", -100)
+      .attr("y", 6)
+      .attr("dy", "-5.1em")
+      .attr("text-anchor", "end")
+      .attr("stroke", "black")
+      .text("value");
 
     const sumstat = d3.group(data, (d) => d.zone_1);
 
-    svgElement
-      .selectAll(".line")
+    g.selectAll(".line")
       .append("g")
       .attr("class", "line")
       .data(sumstat.values())
